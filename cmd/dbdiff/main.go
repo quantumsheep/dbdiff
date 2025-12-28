c4ec5fd7ebf05491a8efd9bf8326e740003ffc2f
Nathanael DEMACON <quantumsheep@users.noreply.github.com>
2025-12-28 12:01:40 +0100

    feat(cli): add dbdiff cli
    Signed-off-by: Nathanael DEMACON <quantumsheep@users.noreply.github.com>

---
+++ cmd/dbdiff/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"slices"
+
+	"github.com/quantumsheep/dbdiff/internal/drivers"
+	"github.com/urfave/cli/v3"
+)
+
+func main() {
+	cmd := &cli.Command{
+		Name:        "dbdiff",
+		Description: "Compare database schemas and generate migration scripts",
+		Action:      action,
+		UsageText:   "dbdiff [global options] <url1> <url2>",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:  "driver",
+				Usage: "Database driver to use. Supported drivers: sqlite3",
+				Validator: func(s string) error {
+					if slices.Contains([]string{"sqlite3"}, s) {
+						return nil
+					}
+					return fmt.Errorf("unsupported driver: %s", s)
+				},
+			},
+		},
+		Arguments: []cli.Argument{
+			&cli.StringArg{
+				Name:      "source",
+				UsageText: "Database connection URL or path for the source database",
+			},
+			&cli.StringArg{
+				Name:      "target",
+				UsageText: "Database connection URL or path for the target database",
+			},
+		},
+	}
+	cmd.Run(context.Background(), os.Args)
+}
+
+func action(ctx context.Context, cmd *cli.Command) error {
+	sourceDatabaseURL := cmd.Args().Get(0)
+	if sourceDatabaseURL == "" {
+		return fmt.Errorf("source database URL is required")
+	}
+
+	targetDatabaseURL := cmd.Args().Get(1)
+	if targetDatabaseURL == "" {
+		return fmt.Errorf("target database URL is required")
+	}
+
+	var driver drivers.Driver
+	var err error
+
+	driverFlag := cmd.String("driver")
+	if driverFlag == "" {
+		driverFlag = "sqlite3"
+	}
+
+	switch driverFlag {
+	case "sqlite3":
+		driver, err = drivers.NewSQLiteDriver(&drivers.SQLLiteDriverConfig{
+			SourceDatabasePath: sourceDatabaseURL,
+			TargetDatabasePath: targetDatabaseURL,
+		})
+		if err != nil {
+			return fmt.Errorf("failed to create sqlite3 driver: %w", err)
+		}
+	default:
+		return fmt.Errorf("unsupported driver: %s", cmd.String("driver"))
+	}
+	defer driver.Close()
+
+	diff, err := driver.Diff(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to diff databases: %w", err)
+	}
+
+	fmt.Println(diff)
+
+	return nil
+}
