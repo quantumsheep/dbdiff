dd973c80667b76d51a7367ce0c72f34ef49a0697
Nathanael DEMACON <quantumsheep@users.noreply.github.com>
2026-08-28 13:57:17 +0200

    test(drivers): add the tests of QuoteIdentifier

---
+++ drivers/identifier_test.go
@@ -0,0 +1,25 @@
+package drivers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestQuoteIdentifier(t *testing.T) {
+	t.Run("PlainName", func(t *testing.T) {
+		require.Equal(t, `"users"`, QuoteIdentifier("users"))
+	})
+
+	t.Run("NameWithAQuote", func(t *testing.T) {
+		require.Equal(t, `"we""ird"`, QuoteIdentifier(`we"ird`))
+	})
+
+	t.Run("NameWithASpace", func(t *testing.T) {
+		require.Equal(t, `"order list"`, QuoteIdentifier("order list"))
+	})
+
+	t.Run("List", func(t *testing.T) {
+		require.Equal(t, []string{`"a"`, `"b"`}, QuoteIdentifiers([]string{"a", "b"}))
+	})
+}
